69b536c5a10ae02a527bfb3977a3f0d0010bad5b
itsikram <mdikram295@gmail.com>
2025-03-09 21:24:40 +0600

    Create All Plugin Files

---
+++ sentiment-analyzer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Sentiment Analyzer
+ * Description: Analyze post content for sentiment and display a sentiment badge.
+ * Version: 1.0
+ * Author: Md Ikram
+ * Author Uri: http://programmerikram.com
+ * Text Domain: sentiment-analyzer
+ */
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
