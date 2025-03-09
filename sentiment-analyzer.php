ad9efddc2c1b7a73f853cae8c209712f8c62eac6
itsikram <mdikram295@gmail.com>
2025-03-09 21:46:05 +0600

    Add Default Keywords

--- sentiment-analyzer.php
+++ sentiment-analyzer.php
@@ -13,3 +13,23 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
+// sa Default Keywords
+$sa_dafault_keywords = array(
+    'positive' => array( 'good', 'happy', 'excellent', 'amazing' ),
+    'negative' => array( 'bad', 'sad', 'terrible', 'awful' ),
+    'neutral' => array( 'normal', 'average' )
+);
+
+// Sentiment analysis on save posts
+
+function sa_analyze_sentiment_on_save_post($post_id, $post ) {
+
+    if(defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+     return;
+    }
+
+}
+
+
+// Call Save Post hook
+add_action('save_post','sa_analyze_sentiment_on_save_post');
